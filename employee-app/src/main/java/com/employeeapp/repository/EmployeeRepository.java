8515fec002c932bed182dac9df07a64cdec3afb5
Code Kalam <rajkalam123@gmail.com>
2025-12-10 07:06:54 +0530

    CRUD project with frontend and backend

--- employee-app/src/main/java/com/employeeapp/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employeeapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employeeapp.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-	
-}
